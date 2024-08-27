370ccf296ee83e25a800423f93ff3888e39c9ac3
Ysoo96 <yeon96@dongyang.ac.kr>
2024-08-27 11:28:01 +0900

    springParamProc

---
+++ springParamProc/src/main/java/com/javateam/springParamProc/SpringParamProcApplication.java
@@ -0,0 +1,13 @@
+package com.javateam.springParamProc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringParamProcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringParamProcApplication.class, args);
+	}
+
+}
---
+++ springParamProc/src/main/java/com/javateam/springParamProc/domain/MemberVO.java
@@ -0,0 +1,23 @@
+package com.javateam.springParamProc.domain;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+// @Data
+public class MemberVO {
+
+	private String name;
+
+	/*
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	*/
+}
